e517f908b38c867bd57929b8e092eaeb5e9b680e
mvila <mvila@voxelgroup.net>
2022-01-01 19:49:48 +0100

    feat: for any position when move forward it will move to that position in the y axis

--- MarsRoverGuided/MarsRoverGuided/MarsRover.cs
+++ MarsRoverGuided/MarsRoverGuided/MarsRover.cs
@@ -1,8 +1,13 @@
-﻿namespace MarsRoverGuided
+﻿using System;
+using System.Globalization;
+
+namespace MarsRoverGuided
 {
     public class MarsRover
     {
         private readonly string _initialState;
+        private int x;
+        private int y;
 
         public MarsRover(string initialState)
         {
@@ -11,8 +16,12 @@
 
         public object Execute(string commands)
         {
+            Int32.TryParse(_initialState.Split(":")[0], out x);
+            Int32.TryParse(_initialState.Split(":")[1], out y);
+            string direction = _initialState.Split(":")[2];
+
             if (commands == "M")
-                return "0:1:N";
+                return $"{x}:{y+1}:{direction}";
             return _initialState;
         }
     }
